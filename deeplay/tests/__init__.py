df89342c8da1ef2818b43083dfb1319105b6c545
Benjamin Midtvedt <benmid@student.chalmers.se>
2024-12-09 09:40:44 +0100

    Add unit tests for torch.backends.mps availability

---
+++ deeplay/tests/__init__.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest.mock import patch
+import torch
+
+_patcher = None
+
+
+def setUpModule():
+    global _patcher
+    _patcher = patch("torch.backends.mps.is_available", return_value=False)
+    _patcher.start()
+
+
+def tearDownModule():
+    global _patcher
+    _patcher.stop()
+
+
+class TestMyModule(unittest.TestCase):
+
+    def test_feature_one(self):
+        self.assertFalse(torch.backends.mps.is_available())
+
+
+class TestAnotherModule(unittest.TestCase):
+
+    def test_feature_two(self):
+        self.assertFalse(torch.backends.mps.is_available())
